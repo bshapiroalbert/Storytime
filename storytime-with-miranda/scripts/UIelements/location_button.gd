87691f7ce642d5b671d01aacc1ca173ef92c3512
Brent Shapiro-Albert <33037158+bshapiroalbert@users.noreply.github.com>
2025-03-02 19:54:42 -0600

    Started connecting location buttons to starting dialog

--- storytime-with-miranda/scripts/UIelements/location_button.gd
+++ storytime-with-miranda/scripts/UIelements/location_button.gd
@@ -4,6 +4,9 @@ class_name LocationButton
 
 signal start_dialog()
 
+func _ready() -> void:
+	start_dialog.connect(TestDialogSystem.start_dialog)
+
 func _on_pressed():
 	print("button pressed")
 	start_dialog.emit()
